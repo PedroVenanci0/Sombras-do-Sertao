3ef90e8143ab6a86d349b0b6e1083e34e32c1c8e
NicolasRaf <nicolasalves136@gmail.com>
2024-07-22 18:04:38 -0300

    Add: new assests of ambience

--- scripts/titlescreen.gd
+++ scripts/titlescreen.gd
@@ -2,15 +2,11 @@ extends Control
 
 @onready var startButton = $Buttons/StartButton
 
-
-func _ready():
-	startButton.grab_focus()
-
 func startPressed():
 	Songs.impactPlay()
-	#var _tween = get_tree().create_tween()
-	#await _tween.tween_property($TransitionCam,"zoom",Vector2(8,8),1).finished
-	Global.fadeTransition("World")
+	$TransitionCam.shakeCamera()
+	var _tween = get_tree().create_tween()
+	_tween.tween_property(self,"modulate",Color(1,1,1,0),3)
 
 func exitPressed():
 	get_tree().quit()
