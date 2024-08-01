89309c15c9c8940f8dad7eca4259e03c4bbde410
ArthurV10 <arthurvms10@gmail.com>
2024-08-01 11:38:46 -0300

    feat: creat jumpscare cacto

--- scripts/deathzone.gd
+++ scripts/deathzone.gd
@@ -9,7 +9,18 @@ func _on_body_entered(body):
 		if get_parent().name == "Monster":
 			get_parent().visible = false
 			add_child(jumpscare)
-		
+			
+		elif get_parent().name == "Cacto":
+			var _camera = get_parent().get_parent().get_parent().get_node("Camera2D")
+			var _tween = get_tree().create_tween()
+			Global.speed = 0
+			Global.cameraShake = true
+			Global.inEscape = false
+			body.SPEED = 0
+			await _tween.tween_property(_camera,"zoom",Vector2(3.0,3.0),1.0).set_trans(Tween.TRANS_CUBIC).finished
+			add_child(jumpscare)
+			
 		body.isDead = true
 		await get_tree().create_timer(1).timeout
 		add_child(gameOver)
+		
