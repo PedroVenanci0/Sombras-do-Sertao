7e0d74fdc2882ade2fb750d04138e7a1b1876382
NicolasRaf <nicolasalves136@gmail.com>
2024-08-05 19:37:49 -0300

    fix: Game muted

--- scripts/random_world.gd
+++ scripts/random_world.gd
@@ -8,7 +8,7 @@ var firstMap : bool = true
 var cameraSpeed = 200
 
 func _ready() -> void:
-	AudioServer.set_bus_volume_db(2,-7)
+	AudioServer.set_bus_volume_db(2,-4)
 	var _tween = get_tree().create_tween()
 	
 	_tween.tween_property($Label,"modulate", Color(1,1,1,0),3).set_trans(Tween.TRANS_BACK).set_delay(2)
